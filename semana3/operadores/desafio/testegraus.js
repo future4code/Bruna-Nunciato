527c4458938d469ed43e44ba4440e8574cea8fd4
bruxxi <bruna_nunciato@hotmail.com>
2021-04-22 08:53:09 -0300

    desafios

---
+++ semana3/operadores/desafio/testegraus.js
@@ -0,0 +1,56 @@
+// desafio 1  -----fahrenheit Kelvin e Celsius-------
+// let valorf = Number(prompt("Digite em Fahrenheit para converter em Kelvin"))
+// console.log (valorf)
+// const calculo1 = (valorf - 32) * 5 / 9 
+// let somafinal = calculo1 + 273.15
+// console.log(`convertendo ${valorf} Fahrenheit fica ${somafinal} graus Kelvin`)
+
+// let celsius = Number( prompt("primeiro valor de graus celsius "))
+// let grauscparaf = (celsius*9) /5 + 32
+// console.log ("neste caso deu " + celsius + " graus celsius fica " + grauscparaf +" graus fahreneit" )
+// let novof = (grauscparaf -32) * 5 / 9
+// let somaf = novof + 273.15
+// console.log(`convertendo ${celsius} graus celsius fica ${somaf} graus Kelvin`)
+
+// let outrocelsius = Number( prompt("mais um valor de graus celsius para convertermos"))
+// let grauscparaoutrof = (outrocelsius*9) /5 + 32
+// console.log ("neste caso deu " + outrocelsius + " graus celsius fica " + grauscparaoutrof +" graus fahreneit" )
+// let novooutrof = (grauscparaoutrof -32) * 5 / 9
+// let somaoutrof = novooutrof + 273.15
+// console.log(`convertendo ${outrocelsius} graus celsius fica ${somaoutrof} graus Kelvin`)
+
+// desafio 2 -----quilowatts horas para R$------
+
+// let quilowatts= Number(prompt("Quantos quilowatts foram consumidos no mês?"))
+// pagar = quilowatts * 0.05
+// console.log(`no caso de consumir ${quilowatts} quilowatts o valor a ser pago será R$ ${pagar}.`)
+// desconto = pagar * 0.85 
+// console.log(`Se conseguir um desconto de 15% sobre a fatura então pagará R$ ${desconto}`)
+
+// desafio 3 -------Libras oz e quilogramas------
+// let libras= Number(prompt("quantidade em Libras"))
+// quilos = libras * 0.453592
+// oz = libras * 16
+// console.log(`${libras} libras é igual a ${quilos} quilos e ${oz} oz (onças) `)
+// let onca = Number (prompt("quantidade em oz"))
+// kilogram = onca * 0.0283495
+// lb = onca / 16
+// console.log(`${onca} oz é igual a ${kilogram} quilos e ${lb} libras`)
+
+
+// desafio c --------- milhas em metros ---------- ft em mt - gal para l--- xicaras  em litros--------
+
+// let mi = Number(prompt("quantas milhas serão convertidas ?"))
+// metros = mi * 1609.34
+// console.log(`${mi} milhas são iguais a ${metros} metros`)
+// let ft = Number(prompt("quantos ft serão convertidos?"))
+// mt = ft * 0.3048
+// console.log(`${ft} pés (ft) são iguais a ${mt} metros`)
+
+// let gal = Number(prompt("quantidade em galão"))
+// lits = gal * 3.785
+
+// let xi= Number(prompt("quantas xicaras quer converter?"))
+// lit = xi * 0.24
+// console.log(`${xi} xicaras é igual a ${lit} litros`)
+
