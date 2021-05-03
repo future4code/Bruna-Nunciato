9cc2a029fe29cc9c9984103da8565c6925f6779b
bruxxi <bruna_nunciato@hotmail.com>
2021-05-02 21:21:37 -0300

    exercicio 11

--- semana4/lista-de-exercicios/exercicios.js
+++ semana4/lista-de-exercicios/exercicios.js
@@ -158,18 +158,18 @@ function segundoMaiorEMenor(array) {
 //Exercício 11
 
 function ordenaArray(array) {
-  // let len = array.length;
-  
-  // for (let i = 0; i < len; i++) {
-  //   for (let j = 0; i < len; i++) {
-  //     if (array[j] > array[j + 1]) {
-  //       let bubble = array[j];
-  //       array[j] = array[j + 1];
-  //       array[j + 1] = bubble;
-  //     }
-  //   }
-  // }
-  // return array
+  let len = array.length;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (array[j] > array[j + 1]) {
+        let bubble = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = bubble;
+      }
+    }
+  }
+  return array;
 }
   
   
