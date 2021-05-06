4676deec40ceea1153aebcf654a9582d458ce7f2
bruxxi <bruna_nunciato@hotmail.com>
2021-05-06 16:35:40 -0300

    exercicio renderizacao

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -10,25 +10,95 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
-  render() {
+  state={
+    pessoa:[
+      {
+      nomeUsuario: 'paulinha',
+      fotoUsuario:'https://picsum.photos/50/50',
+      fotoPost:'https://picsum.photos/200/150'
+    },
+      {
+      nomeUsuario:'Zorii',
+      fotoUsuario:'https://picsum.photos/400/400?a=2',
+      fotoPost:'https://picsum.photos/400/400?a=5'
+    },
+      {
+      nomeUsuario:'Kylo',
+      fotoUsuario:'https://picsum.photos/400/400?a=7',
+      fotoPost:'https://picsum.photos/400/400?a=2'
+    }
+  ],
+  valorInputUsuario:"",
+  valorInputFotoUsuario:"",
+  valorInputFotoPostagem:""
+  }
+
+  adicionaPost = () => {
+    const novaPessoa = {
+      nomeUsuario: this.state.valorInputPessoa,
+      fotoUsuario: this.state.valorInputFotoPerfil,
+fotoPost: this.state.valorInputFotoPostagem
+    };
+    const novoPost = [...this.state.pessoa, novaPessoa];
+
+
+    this.setState({ pessoas: novaPessoa });
+  };
+
+  onChangeInputPessoa = (event) => {
+
+    this.setState({ valorInputPessoa: event.target.value });
+  };
+
+  onChangeInputFotoPerfil = (event) => {
+    this.setState({ valorInputFotoPerfil: event.target.value });
+  };
+onChangeInputFotoPostagem = (event) => {
+    this.setState({ valorInputFotoPostagem: event.target.value });
+  };
+
+
+
+  render (){
+    const listaDePostagem = this.state.pessoa.map((pessoa)=>{
+      
+      return(
+        <div>
+        <input     
+        value={this.state.valorInputUsuario}
+        onChange={this.onChangeInputPessoa}
+        placeholder={"Nome"}
+       />
+       
+        <input
+         value={this.state.valorInputFotoUsuario}
+         onChange={this.onChangeInputFotoPerfil}
+         placeholder={"Foto Perfil"}
+       />
+       
+       <input
+         value={this.state.valorInputFotoPostagem}
+         onChange={this.onChangeInputFotoPostagem}
+         placeholder={"Foto Postagem"}
+         />
+         
+       <button onClick={this.adicionaPost}>Postar</button>
+
+        
+        <Post
+          nomeUsuario={pessoa.nomeUsuario}   
+          fotoUsuario={pessoa.fotoUsuario}
+          fotoPost={pessoa.fotoPost}
+        />
+        </div>
+      )
+    })
+    
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'Zorii'}
-          fotoUsuario={'https://picsum.photos/400/400?a=2'}
-          fotoPost={'https://picsum.photos/400/400?a=5'}
-        />
-         <Post
-          nomeUsuario={'Kylo'}
-          fotoUsuario={'https://picsum.photos/400/400?a=7'}
-          fotoPost={'https://picsum.photos/400/400?a=2'}
-        />
-      
+        {listaDePostagem}
+        
+             
       </MainContainer>
     );
   }
--- semana5/insta4/src/components/Post/Post.js
+++ semana5/insta4/src/components/Post/Post.js
@@ -2,13 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 
 import {IconeComContador} from '../IconeComContador/IconeComContador'
-import {IconeSalvar} from '../IconeSalvar/IconeSalvar'
+
 import iconeCoracaoBranco from '../../img/favorite-white.svg'
 import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
 import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
-import iconeBandeiraBranca from '../../img/bandeiraBranca.svg'
-import iconeBandeiraPreta from '../../img/bandeiraPreta.svg'
 
 const PostContainer = styled.div`
   border: 1px solid gray;
@@ -42,116 +40,84 @@ const PostPhoto = styled.img`
   width: 100%;
 `
 
-
 class Post extends React.Component {
   state = {
     curtido: false,
     numeroCurtidas: 0,
     comentando: false,
-    numeroComentarios: 0,
-    salvo: false,
-    contador: 0,
-  };
-
-  onClickSalvar = () => {
-    this.setState({
-      salvo: !this.state.salvo
-    })
-    if (this.state.contador === 0){
-      this.setState({
-        salvo: true,
-        contador: this.state.contador +1,
-        })
-     } else {
-       this.setState({
-         salvo: false,
-         contador: this.state.contador -1,
-       })
-     }
-    }
-  
+    numeroComentarios: 0
+  }
 
   onClickCurtida = () => {
+    console.log('Curtiu!') 
+    if (!this.state.curtico == true){   
     this.setState({
-      curtido: !this.state.curtido
+      curtido:true,
+      numeroCurtidas: this.state.numeroComentarios +1,
     })
-    
-    if (this.state.numeroCurtidas === 0) {
-      this.setState({
-        curtido: true,
-        numeroCurtidas: this.state.numeroCurtidas + 1,
-      })
-    } else {
-      this.setState({
-        curtido: false,
-        numeroCurtidas: this.state.numeroCurtidas - 1,
-      })
-    }
+  //   else (!this.state.curtido == false)
+  //     this.setState({
+  //       curtido:false,
+  //       numeroCurtidas: this.state.numeroComentarios -1,
+  //   })
+  // }
   }
+}
+
 
   onClickComentario = () => {
     this.setState({
-      comentando: !this.state.comentando,
-    });
-  };
+      comentando: !this.state.comentando
+    })
+  }
 
   aoEnviarComentario = () => {
     this.setState({
       comentando: false,
       numeroComentarios: this.state.numeroComentarios + 1,
-    });
-  };
+    })
+  }
+
 
   render() {
-    let iconeCurtida;
+    let iconeCurtida
 
-    if (this.state.curtido) {
-      iconeCurtida = iconeCoracaoPreto;
+    if(this.state.curtido) {
+      iconeCurtida = iconeCoracaoPreto
     } else {
-      iconeCurtida = iconeCoracaoBranco;
+      iconeCurtida = iconeCoracaoBranco
     }
-    let iconeSalvar;
-    if (this.state.salvo){
-      iconeSalvar = iconeBandeiraPreta;
-    } else {
-      iconeSalvar = iconeBandeiraBranca;
+
+    let componenteComentario
+
+    if(this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
     }
-    let componenteComentario;
-    if (this.state.comentando) {
-      componenteComentario = 
-        <SecaoComentario aoEnviar={this.aoEnviarComentario} />
-      }
 
-    return (
-      <PostContainer>
-        <PostHeader>
-          <UserPhoto src={this.props.fotoUsuario} alt={"Imagem do usuario"} />
-          <p>{this.props.nomeUsuario}</p>
-        </PostHeader>
+    return <PostContainer>
+      <PostHeader>
+        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{this.props.nomeUsuario}</p>
+      </PostHeader>
 
-        <PostPhoto src={this.props.fotoPost} alt={"Imagem do post"} />
+      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
 
-        <PostFooter>
-          <IconeComContador
-            icone={iconeCurtida}
-            onClickIcone={this.onClickCurtida}
-            valorContador={this.state.numeroCurtidas}
-          />
-          <IconeSalvar
-            icone={iconeSalvar}
-            onClickInfo={this.onClickSalvar}
-          />
-          
-          <IconeComContador
-            icone={iconeComentario}
-            onClickIcone={this.onClickComentario}
-            valorContador={this.state.numeroComentarios}
-          />
-        </PostFooter>
-        {componenteComentario}
-      </PostContainer>
-    );
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={this.onClickCurtida}
+          valorContador={this.state.numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={this.onClickComentario}
+          valorContador={this.state.numeroComentarios}
+        />
+      </PostFooter>
+      {componenteComentario}
+    </PostContainer>
   }
 }
 
-export default Post
+export default Post 
