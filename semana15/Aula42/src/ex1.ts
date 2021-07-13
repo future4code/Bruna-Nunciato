7ac200fc363a032964b00a9a685d4250be02ebcb
bruxxi <bruna_nunciato@hotmail.com>
2021-07-13 16:14:23 -0300

    exercicios do dia em andamento

---
+++ semana15/Aula42/src/ex1.ts
@@ -0,0 +1,11 @@
+// const minhaString: string = 1
+
+// a) aparece o erro indicando que number não é uma string atribuível
+
+// const meuNumero: number =1
+// // b) se alterarmos para 
+// let meuOutro: (number| string) = 1
+// meuOutro = "dois"
+
+// c) 
+// let pessoa [] = [nome:string, idade: number,corFavorita:string]
