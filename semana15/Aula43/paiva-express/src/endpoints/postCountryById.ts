c83f1fb5b58ee0044dbb117be5c8b8746f32440b
bruxxi <bruna_nunciato@hotmail.com>
2021-07-14 16:48:39 -0300

    finalizando exercicios

---
+++ semana15/Aula43/paiva-express/src/endpoints/postCountryById.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express"
+import { countries } from "../data"
+import { country } from "../types"
+
+export const postCountryById = (
+   req: Request,
+   res: Response
+): void => {
+    console.log( req.query)
+   const result: country | undefined = countries.find(
+      country => country.id.toString() === req.params.id
+   )
+   
+   if (result) {
+    //    result.country.name
+      
+      res.send("Alteração Efetuada" )
+   } else {
+      res.statusCode = 404
+      res.end()
+   }
+
+}
