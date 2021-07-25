f6eeeda3e6c0cb297148484913e57812c7e74259
bruxxi <bruna_nunciato@hotmail.com>
2021-07-25 19:34:34 -0300

    ok

--- semana16/Projeto/knex-express/src/endpoints/getTaskById.ts
+++ semana16/Projeto/knex-express/src/endpoints/getTaskById.ts
@@ -3,28 +3,63 @@ import moment from "moment";
 import connection from "../connection";
 
 
-export const getTaskById = async (req:Request, res:Response)=>{
-  const id = req.params.id
-  const result = await connection.raw(`
-SELECT T.*, U.nickname FROM Task AS T
-jOIN Users AS U    ON U.id = creatorUserId WHERE T.id=${id}
-`)
-    try {
-
-        if (!result){
-          res.status(404).send({message:"not found" })
-          return 
-              }
-              res.status(200).send(...result,
-                // limitDate:moment(result.limitDate,  `YYYY-MM-DD`).format(`DD/MM/YYYY`)
-                 )
-        }
-        catch (error: any){
-            res.status(400).send({
-                message: error.message || error.sqlMessage
-            })
-        }
+export const getTaskById =  async (req: Request, res: Response) => {
+      try {
+          const result = await connection.raw(`
+          SELECT Task.id, title, description, limitDate, creatorUserId, nickname  
+          FROM Task 
+          JOIN Users ON creatorUserId = Users.id
+          WHERE Task.id = ${req.params.id}
+          `)
+    
+          res.status(200).send(result[0])
+    
+      } catch (err:any) {
+          res.status(400).send({
+              message: err.message
+          })
+      }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// UMA HORA FUNCIONA E OUTRA NÃO
+
+
+
+
+// async (req:Request, res:Response)=>{
+//    const result = await connection.raw(`
+// SELECT T.*, U.nickname FROM Task AS T
+// jOIN Users AS U    ON U.id = creatorUserId WHERE T.id=${req.params.id}
+// `)
+//     try {
+
+//         if (!result){
+//           res.status(404).send({message:"not found" })
+//           return 
+//               }
+//               res.status(200).send(...result,
+//                 // limitDate:moment(result.limitDate,  `YYYY-MM-DD`).format(`DD/MM/YYYY`)
+//                  )
+//         }
+//         catch (error: any){
+//             res.status(400).send({
+//                 message: error.message || error.sqlMessage
+//             })
+//         }
+//     }
 
 
 
--- semana16/Projeto/knex-express/src/index.ts
+++ semana16/Projeto/knex-express/src/index.ts
@@ -1,59 +1,17 @@
-import { Request, Response } from "express";
 import app from "./app";
-import connection from "./connection";
 import { editUserById } from "./endpoints/editUserById";
+import { getTaskById } from "./endpoints/getTaskById";
 import { getUserById } from "./endpoints/getUserById";
 import { postTask } from "./endpoints/postTask";
 import { postUser } from "./endpoints/postUser";
 
 app.post("/user", postUser )
-  
-  app.get("/user/:id", getUserById)
 
+app.get("/user/:id", getUserById)
 
-  app.put("/user/edit/:id", editUserById)
-
+app.put("/user/edit/:id", editUserById)
 
 app.post("/task", postTask)
-  
-   
 
-  // const getTaskById = async (id: string): Promise<any> => {
-  //   const result = await connection.raw(
-  //   `SELECT * FROM Task WHERE id = ${id}`
-  //    );
-  //   return result[0];
-  // };
+app.get("/task/:id", getTaskById)
   
-  // app.get("/task/:id", async (req: Request, res: Response) => {
-  //   try {
-  //     const id = req.params.id;
-  //     const task = await connection.raw(`
-  //     SELECT * FROM Users INNER JOIN Task ON Users.id = ${id};
-  // `);
-  //     res.status(200).send(task);
-  //   } catch (err:any) {
-  //     res.status(500).send({
-  //       message: err.message,
-  //     });
-  //   }
-  // });
-
-
-  app.get("/task/:id", async (req: Request, res: Response) => {
-    try {
-        const result = await connection.raw(`
-        SELECT Task.id, title, description, limitDate, creatorUserId, nickname  
-        FROM Task 
-        JOIN Users ON creatorUserId = Users.id
-        WHERE Task.id = ${req.params.id};
-        `)
-  
-        res.status(200).send(result[0])
-  
-    } catch (err:any) {
-        res.status(400).send({
-            message: err.message
-        })
-    }
-  })
