75d3aabf02524fe6cd7d2dfdb64f87115e37d3d9
bruxxi <bruna_nunciato@hotmail.com>
2021-08-03 16:35:02 -0300

    primeiros exercicios ok

---
+++ semana18/Aula53/respostas.md
@@ -0,0 +1,8 @@
+1)a. Não seria possível porque a propriedade não é publica
+
+b. Apenas uma vez
+
+2) a. classe Customer foi chamado uma vez
+b. Classe User foi impressa duas vezes, pois, foi reutilizado na classe Customer, além da propria classe
+
+3) a. Não é possivel obter a informação do password , pois ela é privada somente para a classe User.
---
+++ semana18/Aula53/src/index.ts
@@ -0,0 +1,75 @@
+import { app } from "./app";
+
+//Exercício 1
+class User {
+    private id: string;
+    private email: string;
+    private name: string;
+    private password: string;
+  
+    constructor(
+          id: string,
+          email: string,
+          name: string,
+          password: string
+      ){
+          console.log("Chamando o construtor da classe User")
+          this.id = id
+          this.email = email
+          this.name = name 
+          this.password = password
+      }
+  
+      public getId(): string {
+          return this.id
+      }
+  
+      public getEmail(): string {
+          return this.email
+      }
+  
+      public getName(): string {
+          return this.name
+      }
+      
+
+  }
+
+
+const novaBruna = new User("8","bru@gmail.com", "Bruna", "12345678");
+
+
+  console.log(novaBruna.getId())
+  console.log(novaBruna.getName())
+  console.log(novaBruna.getEmail())
+
+  //Exercicio 2
+
+class Customer extends User {
+  public purchaseTotal: number = 0;
+  private creditCard: string;
+
+  constructor(
+    id: string,
+    email: string,
+    name: string,
+    password: string,
+    creditCard: string
+  ) {
+    super(id, email, name, password);
+    console.log("Chamando o construtor da classe Customer");
+    this.creditCard = creditCard;
+  }
+
+  public getCreditCard(): string {
+    return this.creditCard;
+  }
+}
+const novoCliente = new Customer("1", "clientinho@gmail.com", "Cliente Sobrenome", "12341234", "visa")
+
+  //Exercicio 3
+
+  console.log(novoCliente.getId())
+  console.log(novoCliente.getName())
+  console.log(novoCliente.getEmail())
+  console.log(novoCliente.getCreditCard())
