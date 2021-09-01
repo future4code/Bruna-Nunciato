3bb019cc43892f57c88b53e68eb5f3e967c36547
bruxxi <bruna_nunciato@hotmail.com>
2021-09-01 14:07:27 -0300

    right branch

---
+++ semana22/brainn-loterias/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Router from './route/Router'
+
+const App = () =>{
+  return(
+    <div className="AppContainer">
+      <Router/>
+    </div>
+  )
+}
+
+export default App;
