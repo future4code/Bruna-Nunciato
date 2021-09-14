0f93691d7943e895aa62622c703e66cf1773e495
bruxxi <bruna_nunciato@hotmail.com>
2021-09-14 17:36:17 -0300

    basic front

--- semana23/Cubo-front/cubo-case/src/components/chart/Chart.js
+++ semana23/Cubo-front/cubo-case/src/components/chart/Chart.js
@@ -1,47 +1,58 @@
-import React from 'react';
-import { Doughnut } from 'react-chartjs-2';
+import React, {useState} from "react";
+import { Doughnut } from "react-chartjs-2";
+import { ContainerChart } from "./styled";
+import { dataList } from "../../mocks/dataMocks";
 
-export default class Chart extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            chartData:{
-                labels: ['Brasil', 'Estados Unidos', 'Belgica', 'Holanda'],
-                datasets:[
-                    {
-                        label: 'Population',
-                        data:[
-                            121,
-                            323,
-                            434,
-                            541
-                        ],
-                        backgroundColor: [
-                            'blue', 'orange', 'blue', 'yellow'
-                        ]
-                    }
-                ]
-            }
-        }
-    }
-    // static defaultProps = {
-    //     displayTitle: true,
-    //     displayLegend: true,
-    //     legendPosition: 'right',
-    //     location: 'City'
-    // }
+const Chart = () => {
+    const [fullName, setFullName] = useState({})
+  const chartData = {
+    labels: ["Brasil", "Estados Unidos", "Belgica", "Holanda"],
+    datasets: [
+      {
+        label: "Population",
+        data: [121, 323, 434, 541],
+        backgroundColor: ["blue", "orange", "blue", "yellow"],
+      },
+    ],
+  };
+//    const fullNames = [];
+//    console.log(dataList)
+//      for (let names of dataList.firstName) {fullNames.push(names);
+//     dataList.forEach((name) => {
+//       fullNames.push(name.lastName);
+//     })}
 
-    render(){
-         return(
-        <div>
-            Chart
-            <Doughnut
-	data={this.state.chartData}
-	options={{ maintainAspectRatio: false }}
-/>
-        </div>
-    )
-    }
-   
+//    dataList.map(data=>{
+//        setFullName(data.firstName)
+//    })
 
-}
+
+  //   chartInfo =  dataList.map(data=>{
+  //       return (  <>
+  //      chartData: [labels: data.firstName ],
+  //       datasets:[
+  //           {
+  //               label: 'Population',
+  //               data:[ data.participation
+  //               ],
+  //               backgroundColor: [
+
+  //               ]}]
+  //       ) </>
+  //    })
+
+  //     // static defaultProps = {
+  //     displayTitle: true,
+  //     displayLegend: true,
+  //     legendPosition: 'right',
+  //     location: 'City'
+  // }
+
+  return (
+    <ContainerChart> {console.log(`fim`, fullName)}
+      Chart
+      <Doughnut data={chartData} options={{ maintainAspectRatio: false }} />
+    </ContainerChart>
+  );
+};
+export default Chart;
--- semana23/Cubo-front/cubo-case/src/components/table/Table.js
+++ semana23/Cubo-front/cubo-case/src/components/table/Table.js
@@ -1,10 +1,34 @@
-export default function Table (){
+import React from "react";
+import { dataList } from '../../mocks/dataMocks'
 
-    return(
-        <div>
-            Table
-        </div>
-    )
-
-    
+class Table extends React.Component {
+ 
+  render() {
+    return (
+      <div>
+        Table
+        <table>
+            <thead>
+            <tr>
+            <th></th>
+              <th>Fisrt Name</th>
+              <th>Last Name</th>
+              <th>Participation</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataList.map((data) => (
+              <tr key={data.firstName}>
+                  <td>{data.id}</td>
+                <td>{data.firstName}</td>
+                <td>{data.lastName}</td>
+                <td>{data.participation}%</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
+export default Table
